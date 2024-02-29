5cd4795ebea7478f69eddc196699fed76ab90142
dmin252 <149593119+dmin252@users.noreply.github.com>
2024-02-29 04:45:09 +0000

    pointCalculation class done

--- src/main/pointCalculation.java
+++ src/main/pointCalculation.java
@@ -1,5 +1,31 @@
 public class pointCalculation {
     public static int calculatePoints(String[] roll) {
-        return 1;
+        int totalScore = 0;
+        if ((roll[0].equals("No Dot") && roll[1].equals("Dot")) || (roll[1].equals("No Dot") && roll[0].equals("Dot"))) {
+            return totalScore;
+        }
+
+        if (roll[0].equals("Trotter") || roll[0].equals("Razorback")) {
+            totalScore += 5;
+        } else if (roll[0].equals("Snouter")) {
+            totalScore += 10;
+        } else if (roll[0].equals("Jowler")) {
+            totalScore += 15;
+        }
+
+        if (roll[1].equals("Trotter") || roll[1].equals("Razorback")) {
+            totalScore += 5;
+        } else if (roll[1].equals("Snouter")) {
+            totalScore += 10;
+        } else if (roll[1].equals("Jowler")) {
+            totalScore += 15;
+        }
+
+        if (totalScore == 0) {
+            return 1;
+        } else if (roll[0].equals(roll[1])) {
+            totalScore *= 2;
+        }
+        return totalScore;
     }
 }
