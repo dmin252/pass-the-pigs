ea8e1a0a790938f603d2b0f579acbe5693f4ee33
dmin252 <149593119+dmin252@users.noreply.github.com>
2024-02-29 00:09:11 +0000

    Finished Human Players

--- src/main/HumanPlayer.java
+++ src/main/HumanPlayer.java
@@ -10,7 +10,14 @@ public class HumanPlayer extends Player {
         strategy = "User Input";
     }
     public boolean wantsToRoll(int myScore, int handScore, ArrayList<Integer> otherScores, int winningScore) {
-        return true;
+        System.out.println("Are you rolling the pigges? (Type 1 for Yes / 2 for No)");
+        int userInput = scanner.nextInt();
+        if (userInput == 1) {
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 
 
